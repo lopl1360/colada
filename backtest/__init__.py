cc9470f7e9af1f568e8696061d8a0d02c4f2f1fe
lopl1360 <lopl@lopl.net>
2025-08-02 21:54:07 -0700

    Add trade and equity tracking with performance metrics (#21)

--- backtest/__init__.py
+++ backtest/__init__.py
@@ -2,11 +2,18 @@
 
 from .backtest_engine import BacktestEngine
 from .trade_simulator import TradeSimulator
-from .metrics import calculate_metrics, plot_performance
+from .metrics import (
+    calculate_metrics,
+    log_trade,
+    plot_performance,
+    track_equity,
+)
 
 __all__ = [
     "BacktestEngine",
     "TradeSimulator",
     "calculate_metrics",
     "plot_performance",
+    "track_equity",
+    "log_trade",
 ]
